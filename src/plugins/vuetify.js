7fe1020eaf574ee4ca848e5d661d98b4b994c57d
Jorge Parra <jorge.luiz.alves.parra@outlook.com>
2020-12-12 22:40:45 -0300

    Adicionei os plugins router, vuetify e vuex

---
+++ src/plugins/vuetify.js
@@ -0,0 +1,21 @@
+import '@mdi/font/css/materialdesignicons.css'
+import Vue from 'vue';
+import Vuetify from 'vuetify/lib/framework';
+import colors from 'vuetify/lib/util/colors'
+
+Vue.use(Vuetify);
+
+export default new Vuetify({
+    theme: {
+        themes: {
+            light: {
+                primary: colors.shades.black,
+                secondary: colors.red.darken4,
+                accent: colors.grey.lighten1,
+            },
+        },
+    },
+    icons: {
+        iconfont: 'mdi',
+    },
+});
